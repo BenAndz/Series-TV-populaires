bc1897664e377ccb7abfa38248895f1a4b1cfd1a
Benjamin Andrieux <Andrieux.Ben@hotmail.fr>
2023-07-11 17:03:31 +0200

    Initial commit

---
+++ src/config.js
@@ -0,0 +1,6 @@
+const BASE_URL = "https://api.themoviedb.org/3/";
+const API_KEY_PARAM = "?api_key=276ab0ec40b641e94f1ee8514fad1099";
+const BACKDROP_BASE_URL = "https://image.tmdb.org/t/p/original/";
+const SMALL_IMG_COVER_BASE_URL = "https://image.tmdb.org/t/p/w300/";
+
+export { BASE_URL, API_KEY_PARAM, BACKDROP_BASE_URL, SMALL_IMG_COVER_BASE_URL };
